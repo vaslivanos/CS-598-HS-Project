87a7ee4bb7cfdc9e92376466cb41d88b022b68a5
Vasileios Livanos <livanos3@linux-a2.ews.illinois.edu>
2017-11-14 23:25:30 -0600

    Reorganization of code and runner scripts

---
+++ Biased_Grid/runner.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+T=100
+N=64
+k=50
+beta=100
+for i in {1..100}; do
+	for Nq in "1" "2" "10"; do
+		for p in "0.01" "0.05" "0.1"; do
+			python -u biased_grid.py $T $N $beta $k $Nq $p &> "Biased_Grid Nq=$Nq p=$p $i" &
+		done
+	done
+done
