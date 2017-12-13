bcdfc4fc298f49a50330f81fde6feb1a1bab5137
GD <gd@GDs-MacBook-Pro.local>
2017-12-12 23:49:13 -0800

    plain and bias 2

--- Biased_Grid/runner.sh
+++ Biased_Grid/runner.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-T=50
-N=36
-k=15
+T=100
+N=64
+k=8
 beta=100
 for i in {1..1}; do
-	for Nq in "10"; do
-		for p in "0.05"; do
+	for Nq in "8"; do
+		for p in "0.1"; do
 			python -u biased_grid.py $T $N $beta $k $Nq $p &> "Biased_Grid Nq=$Nq p=$p $i" &
 		done
 	done
